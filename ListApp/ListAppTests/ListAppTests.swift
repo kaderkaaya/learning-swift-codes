91098a78c0516a8429352e3d344b3630c973094b
kaderkaaya <123060429+kaderkaaya@users.noreply.github.com>
2024-12-17 22:48:47 +0300

    Add files via upload
    I am trying to create a small app example while learning Swift programming.

---
+++ ListApp/ListAppTests/ListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppTests.swift
+//  ListAppTests
+//
+//  Created by Kader Kaya on 17.12.2024.
+//
+
+import Testing
+@testable import ListApp
+
+struct ListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
