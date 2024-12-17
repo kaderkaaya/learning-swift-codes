49816b5cc02e1e7b2071364c78959b1675cc82ec
kaderkaaya <123060429+kaderkaaya@users.noreply.github.com>
2024-12-17 13:09:43 +0300

    Add files via upload
    I learned how to add an image to the project, what a stack view is and how to use it, as well as how to work with Auto Layout.

---
+++ StackAndImageView/StackAndImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackAndImageView
+//
+//  Created by Kader Kaya on 17.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
