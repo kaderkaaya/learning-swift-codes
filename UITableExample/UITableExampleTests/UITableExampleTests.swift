d771185859c41c07aad80c25d2d0d08d19d3ac14
kaderkaaya <123060429+kaderkaaya@users.noreply.github.com>
2024-12-17 12:08:22 +0300

    Add files via upload

---
+++ UITableExample/UITableExampleTests/UITableExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  UITableExampleTests.swift
+//  UITableExampleTests
+//
+//  Created by Kader Kaya on 16.12.2024.
+//
+
+import Testing
+@testable import UITableExample
+
+struct UITableExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
